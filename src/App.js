a6a771a54f4296e87d17d7e2458ad2ad7911b37a
jinxx45 <www.pragat7@pec.edu>
2021-05-20 16:53:25 +0530

    React with Bootstrap Portfolio

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from '../src/components/Navbar/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React  from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Navbar() {
+    return (
+        <>
+              hi
+        </>
+    )
+}
+
+export default Navbar
